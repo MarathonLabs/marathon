0e7aa31468ea9bca2261b6e75625cd2063fe875e
Ivan Balaksha <tagantroy@gmail.com>
2018-04-25 16:35:50 +0800

    fix errors in DeviceStub

--- core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
@@ -1,13 +1,22 @@
 package com.malinskiy.marathon.device
 
+import com.malinskiy.marathon.execution.Configuration
 import com.malinskiy.marathon.test.TestBatch
 
 class DeviceStub(override var operatingSystem: OperatingSystem,
                  override var serialNumber: String,
                  override var networkState: NetworkState,
                  override var healthy: Boolean) : Device {
+    override val model: String
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val manufacturer: String
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val deviceFeatures: Collection<DeviceFeature>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 
-    override fun execute(testBatch: TestBatch) {
-        TODO("not implemented")
+    override fun execute(configuration: Configuration, testBatch: TestBatch) {
+    }
+
+    override fun prepare(configuration: Configuration) {
     }
 }
