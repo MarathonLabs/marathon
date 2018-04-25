ed300b96e1af7a990b752257acb2b4bcd40b0735
Ivan Balaksha <tagantroy@gmail.com>
2018-04-25 16:32:17 +0800

    remove PoolPerDevice strategy and move code from DynamicPoolFactory to Scheduler

--- core/src/main/kotlin/com/malinskiy/marathon/execution/Scheduler.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/execution/Scheduler.kt
@@ -1,5 +1,15 @@
 package com.malinskiy.marathon.execution
 
+import com.malinskiy.marathon.device.DeviceProvider
+import com.malinskiy.marathon.execution.strategy.PoolingStrategy
+import com.malinskiy.marathon.test.Test
+import kotlinx.coroutines.experimental.channels.SendChannel
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import mu.KotlinLogging
+
+
 /**
  * The logic of scheduler
  *
@@ -10,6 +20,65 @@ package com.malinskiy.marathon.execution
  * 5. Batching:     TestBatch into manageable chunks
  * 6. Retries:      Retry if something fails and we didn't account for it in the flakiness
  */
-class Scheduler(val timeout: Int, val batchSize: Int) {
+class Scheduler(private val deviceProvider: DeviceProvider,
+                private val poolingStrategy: PoolingStrategy,
+                private val configuration: Configuration,
+                private val list: Collection<Test>) {
 
+    private val logger = KotlinLogging.logger("DynamicPoolFactory")
+
+    private val pools = mutableMapOf<String, SendChannel<PoolMessage>>()
+
+    suspend fun execute() {
+        launch {
+            delay(10_000)
+            val checkActors = {
+                !pools.values.all { it.isClosedForSend }
+            }
+            while (checkActors()) {
+                delay(1_000)
+            }
+        }.join()
+    }
+
+    private fun subscribeOnDevice() {
+        launch {
+            for (msg in deviceProvider.subscribe()) {
+                when (msg) {
+                    is DeviceProvider.DeviceEvent.DeviceConnected -> {
+                        onDeviceConnected(msg)
+                    }
+                    is DeviceProvider.DeviceEvent.DeviceDisconnected -> {
+                        onDeviceDisconnected(msg)
+                    }
+                }
+            }
+        }
+    }
+
+    fun terminate() {
+        pools.values.forEach {
+            runBlocking {
+                if (!it.isClosedForSend) {
+                    it.send(PoolMessage.Terminate)
+                }
+            }
+        }
+    }
+
+    private suspend fun onDeviceDisconnected(item: DeviceProvider.DeviceEvent.DeviceDisconnected) {
+        pools.values.forEach {
+            it.send(PoolMessage.RemoveDevice(item.device))
+        }
+    }
+
+    private suspend fun onDeviceConnected(item: DeviceProvider.DeviceEvent.DeviceConnected) {
+        val pools = poolingStrategy.createPools(listOf(item.device))
+        pools.forEach {
+            this.pools.computeIfAbsent(it.name, { name ->
+                PoolTestExecutor(name, configuration, list)
+            })
+            this.pools[it.name]?.send(PoolMessage.AddDevice(item.device))
+        }
+    }
 }
