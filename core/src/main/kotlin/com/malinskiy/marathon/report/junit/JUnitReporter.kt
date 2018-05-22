76001504e432c847b505ed47d9ff4eda0372e066
Ivan Balaksha <tagantroy@gmail.com>
2018-05-22 10:28:32 +0800

    extract xml extensions to XmlExtensions file

--- core/src/main/kotlin/com/malinskiy/marathon/report/junit/JUnitReporter.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/report/junit/JUnitReporter.kt
@@ -64,25 +64,3 @@ class JUnitReporter(private val fileManager: FileManager) {
         writer.close()
     }
 }
-
-fun XMLStreamWriter.document(init: XMLStreamWriter.() -> Unit): XMLStreamWriter {
-    this.writeStartDocument()
-    this.init()
-    this.writeEndDocument()
-    return this
-}
-
-fun XMLStreamWriter.element(name: String, init: XMLStreamWriter.() -> Unit): XMLStreamWriter {
-    this.writeStartElement(name)
-    this.init()
-    this.writeEndElement()
-    return this
-}
-
-fun XMLStreamWriter.element(name: String, content: String) {
-    element(name) {
-        writeCharacters(content)
-    }
-}
-
-fun XMLStreamWriter.attribute(name: String, value: String) = writeAttribute(name, value)
