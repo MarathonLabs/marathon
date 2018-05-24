ebed720b6dfc6fc315fc354eba73654d77fcd2b5
Ivan Balaksha <tagantroy@gmail.com>
2018-05-24 12:41:15 +0800

    fix DeviceStub compile time error

--- core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
@@ -9,12 +9,13 @@ class DeviceStub(override var operatingSystem: OperatingSystem,
                  override var healthy: Boolean) : Device {
     override val model: String
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
     override val manufacturer: String
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
     override val deviceFeatures: Collection<DeviceFeature>
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 
-    override suspend fun execute(configuration: Configuration, testBatch: TestBatch) {
+    override suspend fun execute(configuration: Configuration, devicePoolId: DevicePoolId, testBatch: TestBatch) {
     }
 
     override suspend fun prepare(configuration: Configuration) {
