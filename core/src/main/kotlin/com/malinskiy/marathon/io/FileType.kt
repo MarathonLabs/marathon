4f0fa2a125b054a55dbf7d6a99b9be016ffca678
Ivan Balaksha <tagantroy@gmail.com>
2018-05-30 12:47:08 +0800

    add DeviceInfo FileType

--- core/src/main/kotlin/com/malinskiy/marathon/io/FileType.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/io/FileType.kt
@@ -3,5 +3,6 @@ package com.malinskiy.marathon.io
 
 enum class FileType(val dir: String, val suffix: String) {
     TEST("tests", "xml"),
-    LOG("logs", "log")
+    LOG("logs", "log"),
+    DEVICE_INFO("devices", "json")
 }
