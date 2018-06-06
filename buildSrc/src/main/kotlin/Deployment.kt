37c6bdc240a22de8d1d9794a0a9aef0cc8f91937
Ivan Balaksha <tagantroy@gmail.com>
2018-06-06 16:29:22 +0800

    revert changes

--- buildSrc/src/main/kotlin/Deployment.kt
+++ buildSrc/src/main/kotlin/Deployment.kt
@@ -17,8 +17,7 @@ object Deployment {
         val releaseMode: String? by project
         val versionSuffix = when (releaseMode) {
             "RELEASE" -> ""
-            else -> ""
-//            else -> "-SNAPSHOT"
+            else -> "-SNAPSHOT"
         }
 
         Deployment.releaseMode = releaseMode
