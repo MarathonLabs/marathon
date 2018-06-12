1bdbfaed4f94fe19d3aff66e7c66075fcb2c3e26
Ivan Balaksha <tagantroy@gmail.com>
2018-06-12 15:42:38 +0800

    add DeviceStatus and remove unused classes

---
+++ core/src/main/kotlin/com/malinskiy/marathon/execution/DeviceStatus.kt
@@ -0,0 +1,6 @@
+package com.malinskiy.marathon.execution
+
+enum class DeviceStatus {
+    RUNNING,
+    WAITING
+}
--- core/src/main/kotlin/com/malinskiy/marathon/execution/ScheduledBatch.kt
+++
@@ -1,7 +0,0 @@
-package com.malinskiy.marathon.execution
-
-import com.malinskiy.marathon.device.Device
-import com.malinskiy.marathon.test.TestBatch
-
-data class ScheduledBatch(val batch: TestBatch,
-                          val device: Device)
