5163c1e2a1ea8e5c3b53b00ab7aea8cca36657ed
Ivan Balaksha <tagantroy@gmail.com>
2018-06-18 15:52:48 +0800

    count sharding strategy tests

--- core/src/main/kotlin/com/malinskiy/marathon/execution/strategy/impl/sharding/CountShardingStrategy.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/execution/strategy/impl/sharding/CountShardingStrategy.kt
@@ -4,11 +4,10 @@ import com.malinskiy.marathon.execution.TestShard
 import com.malinskiy.marathon.execution.strategy.ShardingStrategy
 import com.malinskiy.marathon.test.Test
 
-class TempShardingStrategy(private val count: Int) : ShardingStrategy {
+class CountShardingStrategy(private val count: Int) : ShardingStrategy {
     override fun createShard(tests: Collection<Test>): TestShard {
         return TestShard(tests.flatMap { test ->
-            (0..count).map { test }
+            (0 until count).map { test }
         })
     }
-
 }
