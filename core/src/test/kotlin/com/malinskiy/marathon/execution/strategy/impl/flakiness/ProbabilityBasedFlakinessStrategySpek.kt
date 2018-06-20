477345dd8fb1f739811018e3ebbc1c297a23a21e
Ivan Balaksha <tagantroy@gmail.com>
2018-06-20 10:51:53 +0800

    fix ProbabilityBasedFlakinessStrategySpek

--- core/src/test/kotlin/com/malinskiy/marathon/execution/strategy/impl/flakiness/ProbabilityBasedFlakinessStrategySpek.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/execution/strategy/impl/flakiness/ProbabilityBasedFlakinessStrategySpek.kt
@@ -7,13 +7,15 @@ import org.amshove.kluent.shouldBe
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import java.time.Instant
 
 class ProbabilityBasedFlakinessStrategySpek : Spek({
     describe("probability-based-strategy test") {
+        val instant = Instant.now()
         group("min success rate 0.8") {
             it("should return 2 flaky tests for one with success rate = 0.5") {
                 val metricsProvider = MetricsProviderStub()
-                val strategy = ProbabilityBasedFlakinessStrategy(0.8)
+                val strategy = ProbabilityBasedFlakinessStrategy(0.8, instant)
                 val testShard = TestShard(TestGenerator().create(1))
                 val result = strategy.process(testShard, metricsProvider)
                 result.tests.size shouldBe 1
@@ -21,7 +23,7 @@ class ProbabilityBasedFlakinessStrategySpek : Spek({
             }
             it("should return one flaky test for one test with success rate = 0.7") {
                 val metricsProvider = MetricsProviderStub(successRate = 0.7)
-                val strategy = ProbabilityBasedFlakinessStrategy(0.8)
+                val strategy = ProbabilityBasedFlakinessStrategy(0.8, instant)
                 val testShard = TestShard(TestGenerator().create(1))
                 val result = strategy.process(testShard, metricsProvider)
                 result.tests.size shouldBe 1
@@ -30,7 +32,7 @@ class ProbabilityBasedFlakinessStrategySpek : Spek({
 
             it("should return three flaky tests for three tests with success rate = 0.7") {
                 val metricsProvider = MetricsProviderStub(successRate = 0.7)
-                val strategy = ProbabilityBasedFlakinessStrategy(0.8)
+                val strategy = ProbabilityBasedFlakinessStrategy(0.8, instant)
                 val testShard = TestShard(TestGenerator().create(3))
                 val result = strategy.process(testShard, metricsProvider)
                 result.tests.size shouldBe 3
