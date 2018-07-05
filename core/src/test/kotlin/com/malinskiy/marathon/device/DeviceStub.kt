9c485f88dc1cace15c3b2076eeefbf6da50507c0
Ivan Balaksha <tagantroy@gmail.com>
2018-07-05 18:18:30 +0800

    remove incorrect import

--- core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/device/DeviceStub.kt
@@ -2,7 +2,7 @@ package com.malinskiy.marathon.device
 
 import com.malinskiy.marathon.analytics.Analytics
 import com.malinskiy.marathon.execution.Configuration
-import com.malinskiy.marathon.execution.TestRunResults
+import com.malinskiy.marathon.execution.RetryMessage
 import com.malinskiy.marathon.execution.progress.ProgressReporter
 import com.malinskiy.marathon.test.TestBatch
 import kotlinx.coroutines.experimental.channels.Channel
@@ -15,7 +15,7 @@ class DeviceStub(override var operatingSystem: OperatingSystem = OperatingSystem
                  override val model: String = "model",
                  override val manufacturer: String = "manufacturer",
                  override val deviceFeatures: Collection<DeviceFeature> = emptyList()) : Device {
-    override fun execute(configuration: Configuration, devicePoolId: DevicePoolId, testBatch: TestBatch, tracker: Analytics, retryChannel: Channel<TestRunResults>, progressReporter: ProgressReporter) {
+    override fun execute(configuration: Configuration, devicePoolId: DevicePoolId, testBatch: TestBatch, tracker: Analytics, retryChannel: Channel<RetryMessage>, progressReporter: ProgressReporter) {
     }
 
     override fun prepare(configuration: Configuration) {
