d77abb8a7a4c1e8ec7d722c0ed7656562e07f25f
Ivan Balaksha <tagantroy@gmail.com>
2018-07-09 19:27:10 +0800

    fix problem when influx is empty and we request data multiple times

--- core/src/main/kotlin/com/malinskiy/marathon/analytics/metrics/remote/influx/InfluxMetricsProvider.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/analytics/metrics/remote/influx/InfluxMetricsProvider.kt
@@ -23,54 +23,57 @@ internal class InfluxMetricsProvider(private val influxDb: InfluxDB,
                                      private val dbName: String) : MetricsProvider {
     private val mapper = InfluxDBResultMapper()
 
-    private val successRate = ConcurrentHashMap<String, Double>()
-    private val executionTime = ConcurrentHashMap<String, Double>()
+    private val successRate = mutableMapOf<String, Double>()
+    private val executionTime = mutableMapOf<String, Double>()
+
+    private var successRateInitialized = false
 
     override fun successRate(test: Test, limit: Instant): Double {
-        if (successRate.isEmpty()) {
-            requestAllSuccessRates(limit)
+        if (!successRateInitialized) {
+            requestAllSuccessRates(limit).forEach {
+                val testName = it.testName
+                val mean = it.mean
+                if (testName != null && mean != null) {
+                    successRate[testName] = mean
+                }
+            }
+            successRateInitialized = true
         }
         return successRate[test.toSafeTestName()] ?: 0.0
     }
 
-    private fun requestAllSuccessRates(limit: Instant) {
+    private fun requestAllSuccessRates(limit: Instant): List<SuccessRate> {
         val results = influxDb.query(Query("""
             SELECT MEAN("success")
             FROM "tests"
             WHERE time >= '$limit'
             GROUP BY "testname"
         """, dbName))
-        val mappedResults = mapper.toPOJO(results, SuccessRate::class.java)
-        mappedResults.forEach {
-            val testName = it.testName
-            val mean = it.mean
-            if (testName != null && mean != null) {
-                successRate[testName] = mean
-            }
-        }
+        return mapper.toPOJO(results, SuccessRate::class.java)
     }
+
+    var executionTimeInitialized = false
 
     override fun executionTime(test: Test,
                                percentile: Double,
                                limit: Instant): Double {
-        if (executionTime.isEmpty()) {
-            requestAllExecutionTimes(percentile, limit)
+        if (!executionTimeInitialized) {
+            requestAllExecutionTimes(percentile, limit).forEach {
+                executionTime[it.testName!!] = it.percentile!!
+            }
+            executionTimeInitialized = true
         }
         return executionTime[test.toSafeTestName()] ?: 0.0
     }
 
     private fun requestAllExecutionTimes(percentile: Double,
-                                         limit: Instant) {
-
+                                         limit: Instant): List<ExecutionTime> {
         val results = influxDb.query(Query("""
             SELECT PERCENTILE("duration",$percentile)
             FROM "tests"
             WHERE time >= '$limit'
             GROUP BY "testname"
         """, dbName))
-        val mappedResults = mapper.toPOJO(results, ExecutionTime::class.java)
-        mappedResults.forEach {
-            executionTime[it.testName!!] = it.percentile!!
-        }
+        return mapper.toPOJO(results, ExecutionTime::class.java)
     }
 }
