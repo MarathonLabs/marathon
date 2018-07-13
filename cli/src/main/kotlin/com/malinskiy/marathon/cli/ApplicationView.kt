4f56990bcaeb5532d55e1ff684dfff27113bd60c
Anton Malinskiy <anton@malinskiy.com>
2018-07-14 00:28:09 +0700

    [cli] add version of app to the cli --help output

--- cli/src/main/kotlin/com/malinskiy/marathon/cli/ApplicationView.kt
+++ cli/src/main/kotlin/com/malinskiy/marathon/cli/ApplicationView.kt
@@ -6,11 +6,12 @@ import com.malinskiy.marathon.cli.config.ConfigFactory
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
 import mu.KotlinLogging
-import java.io.File
 
 private val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>) = mainBody {
+fun main(args: Array<String>) = mainBody(
+        programName = "marathon v${BuildConfig.VERSION}"
+) {
     ArgParser(args).parseInto(::MarathonCliConfiguration).run {
         logger.info { "Starting marathon" }
 
