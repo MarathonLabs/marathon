2f355a3104731c20025454342d06b18c3ecb5e52
Ivan Balaksha <tagantroy@gmail.com>
2018-08-02 22:16:07 +0800

    upgrade kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
 
 buildscript {
-    val kotlinVersion = "1.2.51"
+    val kotlinVersion = "1.2.60"
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
