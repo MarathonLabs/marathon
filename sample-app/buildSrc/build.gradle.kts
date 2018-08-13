c9b041d203de4df7e3d8300c5d7f5f43121f8886
Ivan Balaksha <tagantroy@gmail.com>
2018-08-13 21:31:19 +0800

    upgrade gradle for sample-app

--- sample-app/buildSrc/build.gradle.kts
+++ sample-app/buildSrc/build.gradle.kts
@@ -1,13 +1,7 @@
-import org.gradle.kotlin.dsl.`kotlin-dsl`
-
-buildscript {
-    val kotlinVersion = "1.2.60"
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
 plugins {
     `kotlin-dsl`
+}
+
+repositories {
+    jcenter()
 }
