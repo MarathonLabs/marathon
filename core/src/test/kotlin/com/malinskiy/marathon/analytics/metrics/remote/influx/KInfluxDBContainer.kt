edeb46d17d33e2131de0aa4ac8a4f0ac8b790782
Ivan Balaksha <tagantroy@gmail.com>
2018-09-15 21:17:35 +0800

    remove previous test implementations from unit test dir

--- core/src/test/kotlin/com/malinskiy/marathon/analytics/metrics/remote/influx/KInfluxDBContainer.kt
+++
@@ -1,5 +0,0 @@
-package com.malinskiy.marathon.analytics.metrics.remote.influx
-
-import org.testcontainers.containers.InfluxDBContainer
-
-class KInfluxDBContainer : InfluxDBContainer<KInfluxDBContainer>()
