991f266515efd1367f7d2b473739c1fd2677a00c
Anton Malinskiy <anton@malinskiy.com>
2018-11-13 11:14:59 +0700

    Add debug logging of expected duration of test

--- core/src/main/kotlin/com/malinskiy/marathon/execution/strategy/impl/sorting/ExecutionTimeSortingStrategy.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/execution/strategy/impl/sorting/ExecutionTimeSortingStrategy.kt
@@ -2,16 +2,22 @@ package com.malinskiy.marathon.execution.strategy.impl.sorting
 
 import com.malinskiy.marathon.analytics.metrics.MetricsProvider
 import com.malinskiy.marathon.execution.strategy.SortingStrategy
+import com.malinskiy.marathon.log.MarathonLogging
 import com.malinskiy.marathon.test.Test
+import com.malinskiy.marathon.test.toSimpleSafeTestName
 import java.time.Instant
-import java.util.Comparator
+import java.util.*
 
 class ExecutionTimeSortingStrategy(val percentile: Double,
                                    val timeLimit: Instant) : SortingStrategy {
 
+    val logger = MarathonLogging.logger(ExecutionTimeSortingStrategy::class.java.simpleName)
+
     override fun process(metricsProvider: MetricsProvider): Comparator<Test> =
             Comparator.comparingDouble<Test> {
-                metricsProvider.executionTime(it, percentile, timeLimit)
+                val expectedDuration = metricsProvider.executionTime(it, percentile, timeLimit)
+                logger.debug { "Expecting duration of $expectedDuration for ${it.toSimpleSafeTestName()}" }
+                expectedDuration
             }.reversed()
 
     override fun equals(other: Any?): Boolean {
