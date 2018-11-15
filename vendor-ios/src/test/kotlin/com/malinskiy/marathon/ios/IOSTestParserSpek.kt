1f4ca4b9acedfd90e1cff5cd5f396769c3d1756b
Panforov, Yurii (Agoda) <yurii.panforov@agoda.com>
2018-11-15 17:14:30 +0700

    Add missing parameters

--- vendor-ios/src/test/kotlin/com/malinskiy/marathon/ios/IOSTestParserSpek.kt
+++ vendor-ios/src/test/kotlin/com/malinskiy/marathon/ios/IOSTestParserSpek.kt
@@ -40,6 +40,7 @@ object IOSTestParserSpek : Spek({
                             xctestrunPath = xctestrunPath,
                             remoteUsername = "testuser",
                             remotePrivateKey = File("/home/fakekey"),
+                            knownHostsPath = null,
                             remoteRsyncPath = "/remote/rsync",
                             sourceRoot = sourceRoot,
                             debugSsh = false)
