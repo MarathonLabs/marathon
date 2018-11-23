42f44a364974ccf2f1e9062d7c3481685f45e91f
Panforov, Yurii (Agoda) <yurii.panforov@agoda.com>
2018-11-23 12:09:57 +0700

    Fix filtering configuration

--- core/src/main/kotlin/com/malinskiy/marathon/Marathon.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/Marathon.kt
@@ -116,7 +116,9 @@ class Marathon(val configuration: Configuration) {
             configuration.testClassRegexes.all { it.matches(test.clazz) }
         }
 
-        tests = configuration.filteringConfiguration.whitelist.map { it.filter(tests) }.flatten().toSet().toList()
+        if (configuration.filteringConfiguration.whitelist.size > 0) {
+            tests = configuration.filteringConfiguration.whitelist.map { it.filter(tests) }.flatten().toSet().toList()
+        }
         configuration.filteringConfiguration.blacklist.forEach { tests = it.filterNot(tests) }
         return tests
     }
