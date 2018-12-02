b24674cbe1f08b971634fa16fb556219865db355
Panforov, Yurii (Agoda) <yurii.panforov@agoda.com>
2018-12-02 19:09:11 +0700

    Fix debugging JVM parameter

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -17,7 +17,7 @@ application {
     mainClassName = "com.malinskiy.marathon.cli.ApplicationViewKt"
     applicationName = "marathon"
     applicationDefaultJvmArgs = listOf("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044",
-        "-Dkotlinx.coroutines.experimental.debug=onR")
+        "-Dkotlinx.coroutines.debug=on")
 }
 
 distributions {
