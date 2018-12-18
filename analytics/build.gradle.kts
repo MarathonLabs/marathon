0c2049e81bf9f057461ca5b925e01499428e5322
Konstantin Aksenov <konstantin.aksenov@agoda.com>
2018-12-18 13:46:10 +0700

    Add analytics module

---
+++ analytics/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.gradle.api.plugins.ExtensionAware
+import org.junit.platform.gradle.plugin.FiltersExtension
+import org.junit.platform.gradle.plugin.EnginesExtension
+import org.junit.platform.gradle.plugin.JUnitPlatformExtension
+
+plugins {
+    `java-library`
+    id("org.jetbrains.kotlin.jvm")
+    id("org.junit.platform.gradle.plugin")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.apiVersion = "1.3"
+}
+
+dependencies {
+
+}
