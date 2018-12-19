6ddb257c66feb20d6bb2655c7d1bde75cf57324d
Konstantin Aksenov <konstantin.aksenov@agoda.com>
2018-12-19 13:16:49 +0700

    Fix creation for Configuration in tests

--- core/src/test/kotlin/com/malinskiy/marathon/analytics/metrics/MetricsProviderFactorySpek.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/analytics/metrics/MetricsProviderFactorySpek.kt
@@ -35,7 +35,8 @@ class MetricsProviderFactorySpek : Spek({
                     vendorConfiguration = object : VendorConfiguration {
                         override fun testParser(): TestParser? = null
                         override fun deviceProvider(): DeviceProvider? = null
-                    }
+                    },
+                    analyticsTracking = false
             )
             val factory = MetricsProviderFactory(configuration)
             val metricsProvider = factory.create()
