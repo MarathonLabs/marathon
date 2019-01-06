5a87d9a0b8dd1ba347a98845644fef35808e6e84
Ivan Balaksha <tagantroy@gmail.com>
2019-01-06 15:29:33 +0800

    simplify FileAndroidConfiguration

--- cli/src/test/kotlin/com/malinskiy/marathon/cli/args/FileAndroidConfigurationSpek.kt
+++ cli/src/test/kotlin/com/malinskiy/marathon/cli/args/FileAndroidConfigurationSpek.kt
@@ -25,52 +25,50 @@ object FileAndroidConfigurationSpek : Spek({
         val env = File.createTempFile("foo", "bar")
         val sdk = File.createTempFile("android", "sdk")
 
-        context("android sdk") {
-            group("androidSdk is null") {
-                it("should throw Exception if env android sdk also is null") {
-                    assertFailsWith(ConfigurationException::class) {
-                        configuration.toAndroidConfiguration(null)
-                    }
-                }
-                it("should use env android sdk if it is not null") {
-                    assertEquals(env, configuration.toAndroidConfiguration(env).androidSdk)
+        group("androidSdk is null") {
+            it("should throw Exception if env android sdk also is null") {
+                assertFailsWith(ConfigurationException::class) {
+                    configuration.toAndroidConfiguration(null)
                 }
             }
-            group("android sdk is not null") {
-                it("should use android sdk instead of env if both exists") {
-                    assertEquals(sdk, configuration.copy(androidSdk = sdk).toAndroidConfiguration(env).androidSdk)
-                }
+            it("should use env android sdk if it is not null") {
+                assertEquals(env, configuration.toAndroidConfiguration(env).androidSdk)
             }
-            group("test application output") {
-                it("should be null by default") {
-                    assertNull(configuration.toAndroidConfiguration(env).applicationOutput)
-                }
-                it("should be null if provided") {
-                    assertEquals(env, configuration.copy(applicationOutput = env).toAndroidConfiguration(env).applicationOutput)
-                }
+        }
+        group("android sdk is not null") {
+            it("should use android sdk instead of env if both exists") {
+                assertEquals(sdk, configuration.copy(androidSdk = sdk).toAndroidConfiguration(env).androidSdk)
             }
-            group("test application apk") {
-                it("should be equal") {
-                    assertEquals(env, configuration.copy(testApplicationOutput = env).toAndroidConfiguration(env).testApplicationOutput)
-                }
+        }
+        group("test application output") {
+            it("should be null by default") {
+                assertNull(configuration.toAndroidConfiguration(env).applicationOutput)
             }
-            group("auto grant permissions") {
-                it("should be false by default") {
-                    assertEquals(false, configuration.toAndroidConfiguration(env).autoGrantPermission)
-                }
-                it("should be equal") {
-                    assertEquals(false, configuration.copy(autoGrantPermission = false).toAndroidConfiguration(env).autoGrantPermission)
-                    assertEquals(true, configuration.copy(autoGrantPermission = true).toAndroidConfiguration(env).autoGrantPermission)
-                }
+            it("should be null if provided") {
+                assertEquals(env, configuration.copy(applicationOutput = env).toAndroidConfiguration(env).applicationOutput)
             }
-            group("adb init timeout millis") {
-                it("should be 30_000 by default") {
-                    assertEquals(30_000, configuration.toAndroidConfiguration(env).adbInitTimeoutMillis)
-                }
-                it("should be equal") {
-                    val timeout = 500_000
-                    assertEquals(timeout, configuration.copy(adbInitTimeoutMillis = timeout).toAndroidConfiguration(env).adbInitTimeoutMillis)
-                }
+        }
+        group("test application apk") {
+            it("should be equal") {
+                assertEquals(env, configuration.copy(testApplicationOutput = env).toAndroidConfiguration(env).testApplicationOutput)
+            }
+        }
+        group("auto grant permissions") {
+            it("should be false by default") {
+                assertEquals(false, configuration.toAndroidConfiguration(env).autoGrantPermission)
+            }
+            it("should be equal") {
+                assertEquals(false, configuration.copy(autoGrantPermission = false).toAndroidConfiguration(env).autoGrantPermission)
+                assertEquals(true, configuration.copy(autoGrantPermission = true).toAndroidConfiguration(env).autoGrantPermission)
+            }
+        }
+        group("adb init timeout millis") {
+            it("should be 30_000 by default") {
+                assertEquals(30_000, configuration.toAndroidConfiguration(env).adbInitTimeoutMillis)
+            }
+            it("should be equal") {
+                val timeout = 500_000
+                assertEquals(timeout, configuration.copy(adbInitTimeoutMillis = timeout).toAndroidConfiguration(env).adbInitTimeoutMillis)
             }
         }
     }
