f245de169b6592b74b495e2e7b7b0febf2014049
Konstantin Aksenov <aksenov.kostya@gmail.com>
2019-01-11 09:49:39 +0700

    Add cli report (#152)
    * Add CLI report
    
    * Add failed test report to ExecutionResult

--- report/execution-timeline/src/main/kotlin/com/malinskiy/marathon/report/debug/timeline/ExecutionResult.kt
+++ report/execution-timeline/src/main/kotlin/com/malinskiy/marathon/report/debug/timeline/ExecutionResult.kt
@@ -2,5 +2,6 @@ package com.malinskiy.marathon.report.debug.timeline
 
 data class ExecutionResult(val passedTests: Int,
                            val failedTests: Int,
+                           val ignoredTests: Int,
                            val executionStats: ExecutionStats,
                            val measures: List<Measure>)
