4faffe9e690e85ed054283e434b6fac012b78e51
Panforov, Yurii (Agoda) <yurii.panforov@agoda.com>
2019-01-28 16:47:00 +0700

    Add run it tag to measurements

--- core/src/main/kotlin/com/malinskiy/marathon/analytics/tracker/remote/influx/InfluxDbTracker.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/analytics/tracker/remote/influx/InfluxDbTracker.kt
@@ -8,9 +8,14 @@ import com.malinskiy.marathon.execution.TestStatus
 import com.malinskiy.marathon.test.toSafeTestName
 import org.influxdb.InfluxDB
 import org.influxdb.dto.Point
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 internal class InfluxDbTracker(private val influxDb: InfluxDB) : NoOpTracker() {
+
+    private companion object {
+        val runUUID= UUID.randomUUID().toString()
+    }
 
     override fun trackRawTestRun(poolId: DevicePoolId, device: Device, testResult: TestResult) {
         influxDb.write(Point.measurement("tests")
@@ -20,6 +25,7 @@ internal class InfluxDbTracker(private val influxDb: InfluxDB) : NoOpTracker() {
                 .tag("class", testResult.test.clazz)
                 .tag("method", testResult.test.method)
                 .tag("deviceSerial", device.serialNumber)
+                .tag("run", InfluxDbTracker.runUUID)
                 .addField("ignored", if (testResult.isIgnored) 1.0 else 0.0)
                 .addField("success", if (testResult.status == TestStatus.PASSED) 1.0 else 0.0)
                 .addField("duration", testResult.durationMillis())
