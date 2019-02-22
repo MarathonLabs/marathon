73ec81e55b0bc3cc2ae408cbf18b22cfcba68f2a
Ivan Balaksha <tagantroy@gmail.com>
2019-02-22 17:44:49 +0800

    bump version for snapshots 0.4.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    val marathon = System.getenv("DEPLOY_VERSION_OVERRIDE") ?: "0.4.0"
+    val marathon = System.getenv("DEPLOY_VERSION_OVERRIDE") ?: "0.4.1"
 
     val kotlin = "1.3.20"
     val coroutines = "1.1.1"
