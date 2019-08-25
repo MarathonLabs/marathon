f49ef8aff0085783942299bff6dbcae65c3f9121
Anton Malinskiy <anton@malinskiy.com>
2019-08-25 13:57:10 +1000

    Fix imports and move tests from #267

--- core/src/test/kotlin/com/malinskiy/marathon/analytics/metrics/remote/influx/InfluxMetricsProviderSpek.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/analytics/metrics/remote/influx/InfluxMetricsProviderSpek.kt
@@ -12,20 +12,7 @@ import com.nhaarman.mockitokotlin2.whenever
 import org.amshove.kluent.mock
 import org.amshove.kluent.shouldEqualTo
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import java.time.Instant
-import com.malinskiy.marathon.test.Test
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
-import org.amshove.kluent.shouldEqual
-import org.influxdb.InfluxDB
-import org.influxdb.InfluxDBIOException
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import java.io.IOException
 import java.time.Instant
 
 class InfluxMetricsProviderSpek: Spek({
@@ -96,30 +83,6 @@ class InfluxMetricsProviderSpek: Spek({
             whenever(dataStore.requestAllSuccessRates(secondLimit)).thenReturn(secondList)
             provider.successRate(test, secondLimit) shouldEqualTo secondMean
             verify(dataStore).requestAllSuccessRates(eq(secondLimit))
-        }
-    }
-
-    describe("InfluxMetricsProvider") {
-        it("should return default success rate if initialization failed") {
-            val influxDb = mock<InfluxDB>()
-            val provider = InfluxMetricsProvider(influxDb, "test")
-
-            whenever(influxDb.query(any())).thenThrow(InfluxDBIOException(IOException("test exception")))
-
-            val rate = provider.successRate(Test("com.example", "SingleTest", "method", emptyList()), Instant.now())
-
-            rate shouldEqual 0.0
-        }
-
-        it("should return default execution time if initialization failed") {
-            val influxDb = mock<InfluxDB>()
-            val provider = InfluxMetricsProvider(influxDb, "test")
-
-            whenever(influxDb.query(any())).thenThrow(InfluxDBIOException(IOException("test exception")))
-
-            val executionTime = provider.executionTime(Test("com.example", "SingleTest", "method", emptyList()), 95.0, Instant.now())
-
-            executionTime shouldEqual 300_000.0
         }
     }
 })
