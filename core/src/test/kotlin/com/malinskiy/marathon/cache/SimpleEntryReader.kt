072048635d0af2aad17dceb49001fe300c17fa45
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2019-11-27 15:42:52 +0000

    Use UTF-8 in SimpleEntryReader

--- core/src/test/kotlin/com/malinskiy/marathon/cache/SimpleEntryReader.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/cache/SimpleEntryReader.kt
@@ -2,15 +2,15 @@ package com.malinskiy.marathon.cache
 
 import kotlinx.coroutines.io.ByteReadChannel
 import kotlinx.coroutines.io.readRemaining
-import kotlinx.io.charsets.Charset
 import kotlinx.io.core.readBytes
+import java.nio.charset.StandardCharsets
 
 class SimpleEntryReader : CacheEntryReader {
 
     var readInvoked: Boolean = false
     var bytes: ByteArray? = null
     val data: String?
-        get() = bytes?.toString(charset = Charset.defaultCharset())
+        get() = bytes?.toString(charset = StandardCharsets.UTF_8)
 
     override suspend fun readFrom(input: ByteReadChannel) {
         readInvoked = true
