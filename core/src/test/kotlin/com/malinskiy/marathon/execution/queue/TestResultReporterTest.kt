a29db6ebcc8cb5322c1869bb09a9d8b50f0789a5
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2020-01-20 11:28:57 +0000

    Update test result reporter test

--- core/src/test/kotlin/com/malinskiy/marathon/execution/queue/TestResultReporterTest.kt
+++ core/src/test/kotlin/com/malinskiy/marathon/execution/queue/TestResultReporterTest.kt
@@ -5,6 +5,9 @@ import com.malinskiy.marathon.analytics.internal.pub.Track
 import com.malinskiy.marathon.createDeviceInfo
 import com.malinskiy.marathon.device.DevicePoolId
 import com.malinskiy.marathon.execution.Configuration
+import com.malinskiy.marathon.execution.SimpleClassnameFilter
+import com.malinskiy.marathon.execution.StrictRunFilterConfiguration
+import com.malinskiy.marathon.execution.TestFilter
 import com.malinskiy.marathon.execution.TestResult
 import com.malinskiy.marathon.execution.TestShard
 import com.malinskiy.marathon.execution.TestStatus
@@ -79,7 +82,16 @@ object TestResultReporterSpec : Spek(
             addShard(TestShard(listOf(test, test, test)))
         }
 
-        fun filterStrict() = TestResultReporter(
+        fun strictFilterReporter(filter: TestFilter) = TestResultReporter(
+            poolId,
+            analytics,
+            defaultConfig.copy(strictRunFilterConfiguration = StrictRunFilterConfiguration(filter = listOf(filter), runs = 3)),
+            track
+        ).apply {
+            addShard(TestShard(listOf(test, test, test)))
+        }
+
+        fun strictReporter() = TestResultReporter(
             poolId,
             analytics,
             strictConfig,
@@ -141,15 +153,15 @@ object TestResultReporterSpec : Spek(
         given("a reporter with a strict config") {
             on("success - failure - failure") {
                 it("should report failure") {
-                    val filter = filterStrict()
+                    val reporter = strictReporter()
 
                     val r1 = TestResult(test, deviceInfo, TestStatus.PASSED, 0, 1)
                     val r2 = TestResult(test, deviceInfo, TestStatus.FAILURE, 2, 3)
                     val r3 = TestResult(test, deviceInfo, TestStatus.FAILURE, 4, 5)
 
-                    filter.testFinished(deviceInfo, r1)
-                    filter.testFailed(deviceInfo, r2)
-                    filter.testFailed(deviceInfo, r3)
+                    reporter.testFinished(deviceInfo, r1)
+                    reporter.testFailed(deviceInfo, r2)
+                    reporter.testFailed(deviceInfo, r3)
 
                     inOrder(track) {
                         verify(track).test(poolId, deviceInfo, r1, false)
@@ -162,7 +174,7 @@ object TestResultReporterSpec : Spek(
 
             on("failure - success - success") {
                 it("should report failure") {
-                    val filter = filterStrict()
+                    val filter = strictReporter()
 
                     val r1 = TestResult(test, deviceInfo, TestStatus.FAILURE, 0, 1)
                     val r2 = TestResult(test, deviceInfo, TestStatus.PASSED, 2, 3)
@@ -175,6 +187,94 @@ object TestResultReporterSpec : Spek(
                     inOrder(track) {
                         verify(track).test(poolId, deviceInfo, r1, true)
                         verify(track).test(poolId, deviceInfo, r2, false)
+                        verify(track).test(poolId, deviceInfo, r3, false)
+                        verifyNoMoreInteractions(track)
+                    }
+                }
+            }
+        }
+
+        given("a reporter with a strict run filter when test matches filter") {
+            on("success - failure - failure") {
+                it("should report failure") {
+                    val reporter = strictFilterReporter(filter = SimpleClassnameFilter(Regex.fromLiteral(test.clazz)))
+
+                    val r1 = TestResult(test, deviceInfo, TestStatus.PASSED, 0, 1)
+                    val r2 = TestResult(test, deviceInfo, TestStatus.FAILURE, 2, 3)
+                    val r3 = TestResult(test, deviceInfo, TestStatus.FAILURE, 4, 5)
+
+                    reporter.testFinished(deviceInfo, r1)
+                    reporter.testFailed(deviceInfo, r2)
+                    reporter.testFailed(deviceInfo, r3)
+
+                    inOrder(track) {
+                        verify(track).test(poolId, deviceInfo, r1, false)
+                        verify(track).test(poolId, deviceInfo, r2, true)
+                        verify(track).test(poolId, deviceInfo, r3, false)
+                        verifyNoMoreInteractions(track)
+                    }
+                }
+            }
+
+            on("failure - success - success") {
+                it("should report failure") {
+                    val reporter = strictFilterReporter(filter = SimpleClassnameFilter(Regex.fromLiteral(test.clazz)))
+
+                    val r1 = TestResult(test, deviceInfo, TestStatus.FAILURE, 0, 1)
+                    val r2 = TestResult(test, deviceInfo, TestStatus.PASSED, 2, 3)
+                    val r3 = TestResult(test, deviceInfo, TestStatus.PASSED, 4, 5)
+
+                    reporter.testFailed(deviceInfo, r1)
+                    reporter.testFinished(deviceInfo, r2)
+                    reporter.testFinished(deviceInfo, r3)
+
+                    inOrder(track) {
+                        verify(track).test(poolId, deviceInfo, r1, true)
+                        verify(track).test(poolId, deviceInfo, r2, false)
+                        verify(track).test(poolId, deviceInfo, r3, false)
+                        verifyNoMoreInteractions(track)
+                    }
+                }
+            }
+        }
+
+        given("a reporter with a strict run filter when test does not match filter") {
+            on("success - failure - failure") {
+                it("should report success") {
+                    val reporter = strictFilterReporter(filter = SimpleClassnameFilter(Regex.fromLiteral("$^")))
+
+                    val r1 = TestResult(test, deviceInfo, TestStatus.PASSED, 0, 1)
+                    val r2 = TestResult(test, deviceInfo, TestStatus.FAILURE, 2, 3)
+                    val r3 = TestResult(test, deviceInfo, TestStatus.FAILURE, 4, 5)
+
+                    reporter.testFinished(deviceInfo, r1)
+                    reporter.testFailed(deviceInfo, r2)
+                    reporter.testFailed(deviceInfo, r3)
+
+                    inOrder(track) {
+                        verify(track).test(poolId, deviceInfo, r1, true)
+                        verify(track).test(poolId, deviceInfo, r2, false)
+                        verify(track).test(poolId, deviceInfo, r3, false)
+                        verifyNoMoreInteractions(track)
+                    }
+                }
+            }
+
+            on("failure - success - success") {
+                it("should report success") {
+                    val reporter = strictFilterReporter(filter = SimpleClassnameFilter(Regex.fromLiteral("$^")))
+
+                    val r1 = TestResult(test, deviceInfo, TestStatus.FAILURE, 0, 1)
+                    val r2 = TestResult(test, deviceInfo, TestStatus.PASSED, 2, 3)
+                    val r3 = TestResult(test, deviceInfo, TestStatus.PASSED, 4, 5)
+
+                    reporter.testFailed(deviceInfo, r1)
+                    reporter.testFinished(deviceInfo, r2)
+                    reporter.testFinished(deviceInfo, r3)
+
+                    inOrder(track) {
+                        verify(track).test(poolId, deviceInfo, r1, false)
+                        verify(track).test(poolId, deviceInfo, r2, true)
                         verify(track).test(poolId, deviceInfo, r3, false)
                         verifyNoMoreInteractions(track)
                     }
