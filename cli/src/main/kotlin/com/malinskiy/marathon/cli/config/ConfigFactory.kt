4024afc8a4682edc96648f1ddf6cdbecf8dacc3c
Anton Malinskiy <anton@malinskiy.com>
2020-05-16 21:19:11 +1000

    fix(cli): catch base exception of jackson instead of just MismatchedInputException (#373)

--- cli/src/main/kotlin/com/malinskiy/marathon/cli/config/ConfigFactory.kt
+++ cli/src/main/kotlin/com/malinskiy/marathon/cli/config/ConfigFactory.kt
@@ -1,7 +1,7 @@
 package com.malinskiy.marathon.cli.config
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import com.malinskiy.marathon.cli.args.FileAndroidConfiguration
 import com.malinskiy.marathon.cli.args.FileConfiguration
 import com.malinskiy.marathon.cli.args.FileIOSConfiguration
@@ -73,8 +73,8 @@ class ConfigFactory(private val mapper: ObjectMapper) {
         val configWithEnvironmentVariablesReplaced = environmentVariableSubstitutor.replace(configFile.readText())
         try {
             return mapper.readValue(configWithEnvironmentVariablesReplaced, FileConfiguration::class.java)
-        } catch (e: MismatchedInputException) {
-            logger.error { "Invalid config file ${configFile.absolutePath}. Error parsing ${e.targetType.canonicalName}" }
+        } catch (e: JsonProcessingException) {
+            logger.error(e) { "Error parsing config file ${configFile.absolutePath}" }
             throw ConfigurationException(e)
         }
     }
