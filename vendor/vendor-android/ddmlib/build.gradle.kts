92159b62f499adc291a7a7c1fae1c175a11942ac
Anton Malinskiy <anton@malinskiy.com>
2020-12-19 14:26:24 +1100

    feat(kotlin): update target api version to 1.4 (#424)

--- vendor/vendor-android/ddmlib/build.gradle.kts
+++ vendor/vendor-android/ddmlib/build.gradle.kts
@@ -32,7 +32,7 @@ Deployment.initialize(project)
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-    kotlinOptions.apiVersion = "1.3"
+    kotlinOptions.apiVersion = "1.4"
 }
 
 tasks.withType<Test>().all {
