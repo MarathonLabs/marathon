625ecba39e4d27798c4b65b7c3d6fe7e059c772a
Anton Malinskiy <anton@malinskiy.com>
2020-12-24 02:02:36 +1100

    feat(android): rotate screen capture based on initial device rotation

--- vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/executor/listeners/screenshot/ScreenCapturer.kt
+++ vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/executor/listeners/screenshot/ScreenCapturer.kt
@@ -3,13 +3,13 @@ package com.malinskiy.marathon.android.executor.listeners.screenshot
 import com.malinskiy.marathon.android.AndroidDevice
 import com.malinskiy.marathon.android.ScreenshotConfiguration
 import com.malinskiy.marathon.android.exception.CommandRejectedException
+import com.malinskiy.marathon.android.model.Rotation
 import com.malinskiy.marathon.device.DevicePoolId
 import com.malinskiy.marathon.device.toDeviceInfo
 import com.malinskiy.marathon.io.FileManager
 import com.malinskiy.marathon.io.FileType
 import com.malinskiy.marathon.log.MarathonLogging
 import com.malinskiy.marathon.test.Test
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import org.imgscalr.Scalr
@@ -19,6 +19,7 @@ import java.io.IOException
 import java.time.Duration
 import java.util.concurrent.TimeoutException
 import javax.imageio.stream.FileImageOutputStream
+import kotlin.coroutines.coroutineContext
 import kotlin.system.measureTimeMillis
 
 
@@ -30,52 +31,49 @@ class ScreenCapturer(
     private val timeout: Duration
 ) {
 
-    suspend fun start(test: Test) = coroutineScope {
-        val outputStream = FileImageOutputStream(fileManager.createFile(FileType.SCREENSHOT, poolId, device.toDeviceInfo(), test))
-        val writer = GifSequenceWriter(outputStream, TYPE_INT_ARGB, configuration.delayMs, true)
-        var targetOrientation = detectCurrentDeviceOrientation()
-        while (isActive) {
-            val capturingTimeMillis = measureTimeMillis {
-                getScreenshot(targetOrientation)?.let {
-                    if (targetOrientation == UNDEFINED) {
-                        // remember the target orientation
-                        targetOrientation = it.getOrientation()
+    suspend fun start(test: Test) {
+        var outputStream: FileImageOutputStream? = null
+        val writer: GifSequenceWriter? = null
+        try {
+            val outputStream = FileImageOutputStream(fileManager.createFile(FileType.SCREENSHOT, poolId, device.toDeviceInfo(), test))
+            val writer = GifSequenceWriter(outputStream, TYPE_INT_ARGB, configuration.delayMs, true)
+            var targetRotation = detectCurrentDeviceOrientation()
+            while (coroutineContext.isActive) {
+                val capturingTimeMillis = measureTimeMillis {
+                    getScreenshot(targetRotation)?.let {
+                        writer.writeToSequence(it)
                     }
-                    writer.writeToSequence(it)
                 }
+                val sleepTimeMillis = when {
+                    (configuration.delayMs - capturingTimeMillis) < 0 -> 0
+                    else -> configuration.delayMs - capturingTimeMillis
+                }
+                delay(sleepTimeMillis)
             }
-            val sleepTimeMillis = when {
-                (configuration.delayMs - capturingTimeMillis) < 0 -> 0
-                else -> configuration.delayMs - capturingTimeMillis
-            }
-            delay(sleepTimeMillis)
+        } finally {
+            writer?.close()
+            outputStream?.close()
         }
-        writer.close()
-        outputStream.close()
     }
 
-    private fun detectCurrentDeviceOrientation(): Int {
-        /**
-         * `dumpsys input` is too slow for our purposes: by the time we get the response with SurfaceOrientation the test might already have
-         * finished
-         */
-        return UNDEFINED
-    }
+    private fun detectCurrentDeviceOrientation() = device.initialRotation
 
     private suspend fun getScreenshot(targetOrientation: Rotation): RenderedImage? {
         return try {
             val screenshot = device.getScreenshot(timeout)?.let {
-                // in case the orientation of the image is different than the target, rotate by 90 degrees
-                if (it.getOrientation() != targetOrientation) {
-                    Scalr.rotate(it, Scalr.Rotation.CW_90).also { org -> org.flush() }
-                } else {
-                    it
+                /**
+                 * using initial device rotation during the device discovery,
+                 * dumpsys input takes too long to retrieve this in realtime
+                 */
+                when (targetOrientation) {
+                    Rotation.ROTATION_0 -> it
+                    Rotation.ROTATION_180 -> Scalr.rotate(it, Scalr.Rotation.CW_180).also { org -> org.flush() }
+                    Rotation.ROTATION_270 -> Scalr.rotate(it, Scalr.Rotation.CW_270).also { org -> org.flush() }
+                    Rotation.ROTATION_90 -> Scalr.rotate(it, Scalr.Rotation.CW_90).also { org -> org.flush() }
                 }
             } ?: return null
 
-            // the first time the orientation did not settle, use the actual image orientation
-            val resolvedOrientation = if (targetOrientation == UNDEFINED) screenshot.getOrientation() else targetOrientation
-            if (resolvedOrientation == PORTRAIT) {
+            if (screenshot.width < screenshot.height) {
                 Scalr.resize(screenshot, Scalr.Method.SPEED, Scalr.Mode.AUTOMATIC, configuration.width, configuration.height)
             } else {
                 Scalr.resize(screenshot, Scalr.Method.SPEED, Scalr.Mode.AUTOMATIC, configuration.height, configuration.width)
@@ -91,15 +89,7 @@ class ScreenCapturer(
         }
     }
 
-    /** retrieves the orientation of the RenderImage */
-    private fun RenderedImage.getOrientation(): Int {
-        return if (width > height) LANDSCAPE else PORTRAIT
-    }
-
     companion object {
         val logger = MarathonLogging.logger(ScreenCapturer::class.java.simpleName)
-        private const val UNDEFINED = 0
-        private const val PORTRAIT = 1
-        private const val LANDSCAPE = 2
     }
 }
