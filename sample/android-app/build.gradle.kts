e262fdee4f78461214225a2c6b2eac85e1f42cce
Anton Malinskiy <anton@malinskiy.com>
2021-11-05 15:46:18 +1100

    fix(sample): no need for forcing coroutines version anymore

--- sample/android-app/build.gradle.kts
+++ sample/android-app/build.gradle.kts
@@ -6,15 +6,6 @@ buildscript {
     dependencies {
         classpath(BuildPlugins.kotlinPlugin)
         classpath(BuildPlugins.androidGradle)
-        /**
-         * Starting with kotlin plugin 1.3.41 coroutines dependency is not propagated to the classpath of gradle plugin
-         *
-         * e.g.
-         * Caused by: java.lang.NoSuchMethodError: kotlinx.coroutines.channels.ChannelIterator.next()Ljava/lang/Object;
-         *
-         * Hence we need to explicitly add coroutines to our classpath
-         */
-        classpath(Libraries.kotlinCoroutines)
     }
 }
 
