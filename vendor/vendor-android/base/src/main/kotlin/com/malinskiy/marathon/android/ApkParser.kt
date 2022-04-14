063986325396568748a0cc0a88d5ab8f4f5d9546
Anton Malinskiy <anton@malinskiy.com>
2022-04-14 14:22:56 +1000

    feat(android): add sample test for AndroidManifest.xml parsing

--- vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/ApkParser.kt
+++ vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/ApkParser.kt
@@ -19,50 +19,53 @@ class ApkParser {
             val zip = ZipFile(apk)
             val entry = zip.getEntry("AndroidManifest.xml")
             apkInputStream = zip.getInputStream(entry)
-
-            val parser = AXMLParser(apkInputStream)
-            var eventType = parser.type
-
-            var appPackage: String? = null
-            var testPackage: String? = null
-            var testRunnerClass: String? = null
-            while (eventType != AXMLParser.END_DOCUMENT) {
-                if (eventType == AXMLParser.START_TAG) {
-                    val parserName = parser.name
-                    val isManifest = "manifest" == parserName
-                    val isInstrumentation = "instrumentation" == parserName
-                    if (isManifest || isInstrumentation) {
-                        for (i in 0 until parser.attributeCount) {
-                            val parserAttributeName = parser.getAttributeName(i)
-                            if (isManifest && "package" == parserAttributeName) {
-                                testPackage = parser.getAttributeValueString(i)
-                            } else if (isInstrumentation && "targetPackage" == parserAttributeName) {
-                                appPackage = parser.getAttributeValueString(i)
-                            } else if (isInstrumentation && "name" == parserAttributeName) {
-                                testRunnerClass = parser.getAttributeValueString(i)
-                            }
-                        }
-                    }
-                }
-                eventType = parser.next()
-            }
-
-            checkNotNull(testRunnerClass) { "Could not find test runner class." }
-            checkNotNull(testPackage) { "Could not find test application package." }
-            checkNotNull(appPackage) { "Could not find application package." }
-
-            // Support relative declaration of instrumentation test runner.
-            if (testRunnerClass.startsWith(".")) {
-                testRunnerClass = testPackage + testRunnerClass
-            } else if (!testRunnerClass.contains(".")) {
-                testRunnerClass = "$testPackage.$testRunnerClass"
-            }
-
-            return InstrumentationInfo(appPackage, testPackage, testRunnerClass)
+            return parseAndroidManifest(apkInputStream)
         } catch (e: IOException) {
             throw RuntimeException("Unable to parse test app AndroidManifest.xml.", e)
         } finally {
             apkInputStream?.close()
         }
+    }
+
+    fun parseAndroidManifest(input: InputStream): InstrumentationInfo {
+        val parser = AXMLParser(input)
+        var eventType = parser.type
+
+        var appPackage: String? = null
+        var testPackage: String? = null
+        var testRunnerClass: String? = null
+        while (eventType != AXMLParser.END_DOCUMENT) {
+            if (eventType == AXMLParser.START_TAG) {
+                val parserName = parser.name
+                val isManifest = "manifest" == parserName
+                val isInstrumentation = "instrumentation" == parserName
+                if (isManifest || isInstrumentation) {
+                    for (i in 0 until parser.attributeCount) {
+                        val parserAttributeName = parser.getAttributeName(i)
+                        if (isManifest && "package" == parserAttributeName) {
+                            testPackage = parser.getAttributeValueString(i)
+                        } else if (isInstrumentation && "targetPackage" == parserAttributeName) {
+                            appPackage = parser.getAttributeValueString(i)
+                        } else if (isInstrumentation && "name" == parserAttributeName) {
+                            testRunnerClass = parser.getAttributeValueString(i)
+                        }
+                    }
+                }
+            }
+            eventType = parser.next()
+        }
+
+        checkNotNull(testRunnerClass) { "Could not find test runner class." }
+        checkNotNull(testPackage) { "Could not find test application package." }
+        checkNotNull(appPackage) { "Could not find application package." }
+
+        // Support relative declaration of instrumentation test runner.
+        if (testRunnerClass.startsWith(".")) {
+            testRunnerClass = testPackage + testRunnerClass
+        } else if (!testRunnerClass.contains(".")) {
+            testRunnerClass = "$testPackage.$testRunnerClass"
+        }
+
+        return InstrumentationInfo(appPackage, testPackage, testRunnerClass)
     }
 }
--- vendor/vendor-android/base/src/test/kotlin/com/malinskiy/marathon/android/ApkParserTest.kt
+++ vendor/vendor-android/base/src/test/kotlin/com/malinskiy/marathon/android/ApkParserTest.kt
@@ -1,5 +1,6 @@
 package com.malinskiy.marathon.android
 
+import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldEqual
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -8,7 +9,7 @@ import java.io.File
 class ApkParserTest {
 
     @Test
-    fun `should parser AndroidManifest and return InstrumentationInfo`() {
+    fun `should extract and parse AndroidManifest and return InstrumentationInfo`() {
         val parser = ApkParser()
         val apkFile = File(javaClass.classLoader.getResource("android_test_1.apk").file)
         parser.parseInstrumentationInfo(apkFile) shouldEqual InstrumentationInfo(
@@ -16,5 +17,18 @@ class ApkParserTest {
             "com.example.test",
             "android.support.test.runner.AndroidJUnitRunner"
         )
+    }
+
+    @Test
+    fun `should parse AndroidManifest 0`() {
+        val parser = ApkParser()
+        val file = File(javaClass.classLoader.getResource("manifest/manifest-0.xml").file)
+        file.inputStream().use {
+            parser.parseAndroidManifest(it) shouldBeEqualTo InstrumentationInfo(
+                "com.example",
+                "com.example.test",
+                "android.support.test.runner.AndroidJUnitRunner"
+            )
+        }
     }
 }
