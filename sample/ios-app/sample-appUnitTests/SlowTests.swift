aa93bca3b5c870747f61546f5d57ac76095a3121
Anton Malinskiy <anton@malinskiy.com>
2022-12-29 20:06:50 +1000

    feat(ios): add sample unit tests and update project

---
+++ sample/ios-app/sample-appUnitTests/SlowTests.swift
@@ -0,0 +1,58 @@
+//
+//  SlowTests.swift
+//  sample-appUnitTests
+//
+//  Created by Anton Malinskiy on 29/12/2022.
+//  Copyright © 2022 Panforov, Yurii (Agoda). All rights reserved.
+//
+
+import XCTest
+
+final class SlowTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSlow1() {
+        sleep(5)
+    }
+
+    func testSlow2() {
+        sleep(5)
+    }
+
+    func testSlow3() {
+        sleep(5)
+    }
+
+    func testSlow4() {
+        sleep(5)
+    }
+
+    func testSlow5() {
+        sleep(5)
+    }
+
+    func testSlow6() {
+        sleep(5)
+    }
+
+    func testSlow7() {
+        sleep(5)
+    }
+
+
+    func testSlow8() {
+        sleep(5)
+    }
+
+    func testSlow9() {
+        sleep(5)
+    }
+
+}
