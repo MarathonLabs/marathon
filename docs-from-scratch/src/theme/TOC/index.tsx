ca6f4c8b03f706af68f08ceff0cd8396e762433f
Anton Malinskiy <anton@malinskiy.com>
2023-01-13 23:30:41 +1000

    add swizzled components

---
+++ docs-from-scratch/src/theme/TOC/index.tsx
@@ -0,0 +1,19 @@
+import { useLocation } from '@docusaurus/router';
+import { usePluginData } from '@docusaurus/useGlobalData';
+import OriginalTOC from '@theme-original/TOC';
+import EditThisPage from '@theme/EditThisPage';
+import React, { useEffect, useState } from 'react';
+
+export default function TOC({ toc, editUrl, ...props }) {
+  const isEmpty = toc.length <= 0;
+
+  if (isEmpty) return null;
+
+  return (
+    <div className="toc-wrapper">
+      <h2>Contents</h2>
+      <OriginalTOC toc={toc} {...props} />
+      <EditThisPage editUrl={editUrl} />
+    </div>
+  );
+}
