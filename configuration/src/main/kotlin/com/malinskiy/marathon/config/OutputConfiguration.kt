3d928f0395c687fa117aaa510a6c06851c3f36ad
Anton Malinskiy <anton@malinskiy.com>
2024-02-04 17:10:35 +1000

    fix(configuration): add comments to constants

--- configuration/src/main/kotlin/com/malinskiy/marathon/config/OutputConfiguration.kt
+++ configuration/src/main/kotlin/com/malinskiy/marathon/config/OutputConfiguration.kt
@@ -2,7 +2,9 @@ package com.malinskiy.marathon.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+// Value 0 is equivalent to unlimited path length
 const val OUTPUT_MAX_PATH = 0
+// Value 0 is equivalent to unlimited filename length
 const val OUTPUT_MAX_FILENAME = 255
 
 data class OutputConfiguration(
