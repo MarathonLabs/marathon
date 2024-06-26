6c689dbd750591289db15b165b0f74bfea644393
Oleg Karpov <ptax208@gmail.com>
2024-06-26 17:47:58 +0200

    Added Mordant logger interface, Marathonfile parameter

--- configuration/src/main/kotlin/com/malinskiy/marathon/config/Configuration.kt
+++ configuration/src/main/kotlin/com/malinskiy/marathon/config/Configuration.kt
@@ -42,6 +42,7 @@ data class Configuration private constructor(
     val testBatchTimeoutMillis: Long,
     val testOutputTimeoutMillis: Long,
     val debug: Boolean,
+    val prettyTerminalOutput: Boolean,
 
     val screenRecordingPolicy: ScreenRecordingPolicy,
 
@@ -72,6 +73,7 @@ data class Configuration private constructor(
             "testBatchTimeoutMillis" to testBatchTimeoutMillis.toString(),
             "testOutputTimeoutMillis" to testOutputTimeoutMillis.toString(),
             "debug" to debug.toString(),
+            "prettyTerminalOutput" to prettyTerminalOutput.toString(),
             "screenRecordingPolicy" to screenRecordingPolicy.toString(),
             "vendorConfiguration" to vendorConfiguration.toString(),
             "deviceInitializationTimeoutMillis" to deviceInitializationTimeoutMillis.toString()
@@ -103,6 +105,7 @@ data class Configuration private constructor(
         if (testBatchTimeoutMillis != other.testBatchTimeoutMillis) return false
         if (testOutputTimeoutMillis != other.testOutputTimeoutMillis) return false
         if (debug != other.debug) return false
+        if (prettyTerminalOutput != other.prettyTerminalOutput) return false
         if (screenRecordingPolicy != other.screenRecordingPolicy) return false
         if (vendorConfiguration != other.vendorConfiguration) return false
         if (analyticsTracking != other.analyticsTracking) return false
@@ -133,6 +136,7 @@ data class Configuration private constructor(
         result = 31 * result + testBatchTimeoutMillis.hashCode()
         result = 31 * result + testOutputTimeoutMillis.hashCode()
         result = 31 * result + debug.hashCode()
+        result = 31 * result + prettyTerminalOutput.hashCode()
         result = 31 * result + screenRecordingPolicy.hashCode()
         result = 31 * result + vendorConfiguration.hashCode()
         result = 31 * result + analyticsTracking.hashCode()
@@ -164,6 +168,7 @@ data class Configuration private constructor(
          var testBatchTimeoutMillis: Long = DEFAULT_BATCH_EXECUTION_TIMEOUT_MILLIS,
          var testOutputTimeoutMillis: Long = DEFAULT_OUTPUT_TIMEOUT_MILLIS,
          var debug: Boolean = true,
+         var prettyTerminalOutput: Boolean = false,
 
          var screenRecordingPolicy: ScreenRecordingPolicy = ScreenRecordingPolicy.ON_FAILURE,
 
@@ -196,6 +201,7 @@ data class Configuration private constructor(
                 testBatchTimeoutMillis = testBatchTimeoutMillis,
                 testOutputTimeoutMillis = testOutputTimeoutMillis,
                 debug = debug,
+                prettyTerminalOutput = prettyTerminalOutput,
                 screenRecordingPolicy = screenRecordingPolicy,
                 vendorConfiguration = vendorConfiguration,
                 analyticsTracking = analyticsTracking,
